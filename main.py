f7f6cafd3f60802a2766a3ad33e4d1897b4cd34e
Yuichi Tateno <hotchpotch@gmail.com>
2024-01-13 20:24:18 +0900

    Add argparse and datetime imports, modify post_discord function to include date in summary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
+import argparse
 import os
-from urllib.request import Request, urlopen
+from datetime import datetime
 
 import requests
 
@@ -8,8 +8,9 @@ from secon_d_summary.chain import build_chain
 from secon_d_summary.diary_retriever import Diary
 
 
-def post_discord(webhook_url: str, summary: str, diaries: list[Diary]):
-    summary_data = {"content": summary}
+def post_discord(webhook_url: str, summary: str, diaries: list[Diary], dt: datetime):
+    mmdd = dt.strftime("🗒️ %m月%d日の出来事")
+    summary_data = {"content": mmdd + "\r" + summary}
     summary_res = requests.post(webhook_url, json=summary_data)
     if summary_res.status_code == 204:
         fields = []
@@ -25,8 +26,28 @@ def post_discord(webhook_url: str, summary: str, diaries: list[Diary]):
         res = requests.post(webhook_url, json=data)
 
 
+def args_to_dt(args: argparse.Namespace) -> datetime:
+    date = args.date
+    # date を YYYYMMDD 形式に変換
+    if date:
+        # 柔軟にdatetimeに変換する
+        dt = datetime.strptime(date, "%Y%m%d")
+    else:
+        dt = datetime.now()
+        dt = dt.replace(year=dt.year - 1)
+    return dt
+
+
 if __name__ == "__main__":
-    url = "https://secon.dev/entry/2023/01/13/210000/"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--date", help="date in YYYYMMDD format")
+    args = parser.parse_args()
+    dt = args_to_dt(args)
+    date_ymd = dt.strftime("%Y/%m/%d")
+
+    url = f"https://secon.dev/entry/{date_ymd}/210000/"
+
+    print("target_url:", url)
     chain = build_chain()
     result = chain.invoke(url)
     diaries: list[Diary] = result["diaries"]
@@ -35,4 +56,4 @@ if __name__ == "__main__":
     webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
 
     if webhook_url:
-        post_discord(webhook_url, summary, diaries)
+        post_discord(webhook_url, summary, diaries, dt)

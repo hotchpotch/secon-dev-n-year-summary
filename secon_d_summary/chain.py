ab345340748f49cb8fc1d331f4f1b94fd602977d
Yuichi Tateno <hotchpotch@gmail.com>
2024-01-13 17:26:00 +0900

    Add chain decorators and modify build_chain function

--- secon_d_summary/chain.py
+++ secon_d_summary/chain.py
@@ -1,16 +1,41 @@
+from operator import itemgetter
+
 from langchain_core.runnables import (
     ConfigurableField,
     RunnableLambda,
     RunnableParallel,
     RunnablePassthrough,
 )
+from langchain_core.runnables import chain as chain_decorator
 
 from .diary_retriever import Diary, diary_retriever
 
 
+@chain_decorator
+def _filter_none(x):
+    # None なら削除
+    return list(filter(lambda y: y is not None, x))
+
+
+@chain_decorator
+def _combine_diaries(x):
+    return [x["diary"]] + x["n_diaries"]
+
+
+@chain_decorator
+def _do_nothing(x):
+    return None
+
+
 def build_chain():
     retriever = RunnableLambda(diary_retriever)
-    chain = retriever | {
-        "main_diary": RunnablePassthrough(),
-    }
+
+    def _assine_n_diaries(x):
+        return RunnablePassthrough.assign(
+            diary=RunnablePassthrough(),
+            n_diaries=itemgetter("n_diary_urls")
+            | retriever.with_fallbacks([_do_nothing]).map(),
+        )
+
+    chain = retriever | _assine_n_diaries | _combine_diaries | _filter_none
     return chain
